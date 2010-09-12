d4876b6f7073fe964577b4b7afed0b4b49030992
bryanl <bryan@osesm.com>
2010-09-12 11:20:59 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sequel::Application.config.secret_token = '1a96aee8c80fc14e89527e29882a273e962c2ccf50410df8d788ed48830da3d09757602d085467622e124ef8e8aa72e20ffa049c57a6ba7db6e75b12c7545096'
